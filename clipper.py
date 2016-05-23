facf5f23ec606567123a07eea417713570df0af0
Rebecca Dacso <rebeccadacso@Rebeccas-MacBook-Pro.local>
2016-05-23 14:45:39 -0700

    removed unnecessary code from clipper part one

--- clipper.py
+++ clipper.py
@@ -1,16 +1,8 @@
-one = 0
-five =0
-ten = 0
-twenty = 0
 
 def load_card(num_one, num_five, num_ten, num_twenty):
-	global one
 	one = num_one * 1
-	global five
 	five = num_five * 5
-	global ten
 	ten = num_ten * 10
-	global twenty
 	twenty = num_twenty * 20
 	total_money = one + five + ten + twenty
 	return total_money
